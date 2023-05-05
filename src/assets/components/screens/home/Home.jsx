c03411366fbdbe696b87f8c01b148f0a4cacea3a
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-05-05 11:46:42 +0300

    Start

---
+++ src/assets/components/screens/home/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+	return (
+		<>
+			<h1>Cars catalog</h1>
+			<div>
+				<div>
+					<h2>Car 1</h2>
+					<p>$100 000</p>
+					<button>Read more</button>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Home
