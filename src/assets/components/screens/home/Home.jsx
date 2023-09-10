5c2f45d139981c7e357b591ea3bca3e8fda5e3d0
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-09-10 16:43:57 +0300

    my start. I don't remember

--- src/assets/components/screens/home/Home.jsx
+++ src/assets/components/screens/home/Home.jsx
@@ -1,13 +1,23 @@
+import s from './Home.module.css'
+import { cars } from './cars.data'
 function Home() {
 	return (
 		<>
 			<h1>Cars catalog</h1>
 			<div>
-				<div>
-					<h2>Car 1</h2>
-					<p>$100 000</p>
-					<button>Read more</button>
-				</div>
+				{cars.map(car => (
+					<div key={car.id} className={s.item}>
+						<div
+							className={s.image}
+							style={{ backgroundImage: `url(${car.image})` }}
+						/>
+						<div className={s.info}>
+							<h2>Car 1</h2>
+							<p>$100 000</p>
+							<button>Read more</button>
+						</div>
+					</div>
+				))}
 			</div>
 		</>
 	)
---
+++ src/assets/components/screens/home/cars.data.js
@@ -0,0 +1,57 @@
+export const cars = [
+	{
+		id: 1,
+		name: 'Toyota Camry',
+		price: 24970,
+		image: 'http://localhost:4200/images/1.jpg',
+	},
+	{
+		id: 2,
+		name: 'Honda Civic',
+		price: 22545,
+		image: 'http://localhost:4200/images/2.jpg',
+	},
+	{
+		id: 3,
+		name: 'Ford Mustang',
+		price: 28840,
+		image: 'http://localhost:4200/images/3.jpg',
+	},
+	{
+		id: 4,
+		name: 'Chevrolet Corvette',
+		price: 62195,
+		image: 'http://localhost:4200/images/4.jpg',
+	},
+	{
+		id: 5,
+		name: 'Tesla Model S',
+		price: 79990,
+		image: 'http://localhost:4200/images/5.jpg',
+	},
+	{
+		id: 6,
+		name: 'BMW 3 Series',
+		price: 43700,
+		image: 'http://localhost:4200/images/6.webp',
+	},
+	{
+		id: 7,
+		name: 'Audi A4',
+		price: 40145,
+		image: 'http://localhost:4200/images/7.jpg',
+	},
+	{
+		id: 8,
+		name: 'Mercedes-Benz C-Class',
+		price: 43600,
+		image: 'http://localhost:4200/images/8.jpg',
+	},
+	{
+		name: 'McLaren',
+		price: 300000,
+		image:
+			'https://static.thairath.co.th/media/Dtbezn3nNUxytg04agCxzzag39REiDhkFK5yvCZ4iYKG4p.jpg',
+		id: 9,
+	},
+]
