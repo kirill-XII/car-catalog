182c1d636c8987e1f3e55cf3c23b3881679dcb22
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-09-17 16:42:34 +0300

    change img

--- src/assets/components/screens/home/cars.data.js
+++ src/assets/components/screens/home/cars.data.js
@@ -3,55 +3,61 @@ export const cars = [
 		id: 1,
 		name: 'Toyota Camry',
 		price: 24970,
-		image: 'http://localhost:4200/images/1.jpg',
+		image:
+			'https://cdn.motor1.com/images/mgl/0MmJj/s1/toyota-camry-modellista-three-quarters.jpg',
 	},
 	{
 		id: 2,
 		name: 'Honda Civic',
 		price: 22545,
-		image: 'http://localhost:4200/images/2.jpg',
+		image:
+			'https://avatars.dzeninfra.ru/get-zen_doc/1923220/pub_5f9be03d4f93a25b14e3ecc3_5f9be90a6fd57035fdafbf04/scale_1200',
 	},
 	{
 		id: 3,
 		name: 'Ford Mustang',
 		price: 28840,
-		image: 'http://localhost:4200/images/3.jpg',
+		image:
+			'https://sportishka.com/uploads/posts/2021-12/1638997770_12-sportishka-com-p-ford-mustang-sport-krasivo-foto-14.jpg',
 	},
 	{
 		id: 4,
 		name: 'Chevrolet Corvette',
 		price: 62195,
-		image: 'http://localhost:4200/images/4.jpg',
+		image:
+			'http://wp.widewallpapers.ru/2k/cars/chevrolet/corvette/1920x1080/Chevrolet-Corvette-Stingray-Coupe-EU-spec-2013-1920x1080-008.jpg',
 	},
 	{
 		id: 5,
 		name: 'Tesla Model S',
 		price: 79990,
-		image: 'http://localhost:4200/images/5.jpg',
+		image:
+			'https://carsweek.ru/upload/iblock/134/g5ta4ojzjcth7gonabhrnzlpvw1kclyr.jpg',
 	},
 	{
 		id: 6,
 		name: 'BMW 3 Series',
 		price: 43700,
-		image: 'http://localhost:4200/images/6.webp',
+		image: 'https://avtoaziya.ru/images/FOTO/25/BMW_3_Series_2023-2.jpg',
 	},
 	{
 		id: 7,
 		name: 'Audi A4',
 		price: 40145,
-		image: 'http://localhost:4200/images/7.jpg',
+		image: 'https://ic.drivenn.ru/cjo3j0bxh2ncw_ymt2kf.jpeg',
 	},
 	{
 		id: 8,
 		name: 'Mercedes-Benz C-Class',
 		price: 43600,
-		image: 'http://localhost:4200/images/8.jpg',
+		image:
+			'http://mb.carwallpapers.ru/mercedes-benz-usa/c/2012-c350-amg-sp/Mercedes-Benz-C350-AMG-Sports-Package-2012-1920x1080-009.jpg',
 	},
 	{
+		id: 9,
 		name: 'McLaren',
 		price: 300000,
 		image:
 			'https://static.thairath.co.th/media/Dtbezn3nNUxytg04agCxzzag39REiDhkFK5yvCZ4iYKG4p.jpg',
-		id: 9,
 	},
 ]
