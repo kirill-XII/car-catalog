7dc5a0323ec20858b360256aca57aa680741e422
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-09-27 15:37:51 +0300

    useState, cars as carsData, prop setCars

--- src/assets/components/screens/home/Home.jsx
+++ src/assets/components/screens/home/Home.jsx
@@ -1,12 +1,15 @@
 import CarItem from './car-item/CarItem'
-import { cars } from './cars.data'
+import { cars as carsData } from './cars.data'
 import CreateCarForm from './create-car-form/CreateCarForm'
+import { useState } from 'react'
 
 function Home() {
+	const [cars, setCars] = useState(carsData)
+	console.log(cars)
 	return (
 		<>
 			<h1>Cars catalog</h1>
-			<CreateCarForm />
+			<CreateCarForm setCars={setCars} />
 			<div>
 				{cars.map(car => (
 					<CarItem key={car.id} car={car} />
